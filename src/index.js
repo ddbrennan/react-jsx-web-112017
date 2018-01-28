efccbb3819cd7bfa6f1f0bf39bae28cde4e18842
ddbrennan <ddbren@gmail.com>
2018-01-28 15:04:33 -0500

    jsx and import export functioning

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Code goes here
+import Tweet from './components/tweets';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import { message } from './components/foo';
+
+import fruit from './components/fruits';
+
+import { apple } from './components/fruits';
+
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
